6420e5204828fe7c67568124b8313d3c96302a4f
Tomasz Nowak <Tomasz.Nowak@schange.com>
2018-08-07 12:47:13 +0200

    Adding Razor Page Greeting

---
+++ OdeToFood/Pages/Greeting.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Services;
+
+namespace OdeToFood.Pages
+{
+    public class GreetingModel : PageModel
+    {
+        private IGreeter greeter2;
+
+        public string CurrentGreeting { get; set; }
+
+        public GreetingModel(IGreeter greeter)
+        {
+            this.greeter2 = greeter;
+        }
+
+        public void OnGet(string name)
+        {
+            CurrentGreeting = greeter2.GetMessage() + $" You passed parameter {name}";
+
+        }
+    }
+}
